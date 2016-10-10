d3006eb685ad10aae3454c5b515668dbc3111f82
Deepak Jayaram <deepak.jayaram@dealertrack.com>
2016-10-10 10:57:42 -0400

    Live editing with state example

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import './Home.css';
-
+import './Home.css'
 
 class Home extends React.Component {
 
@@ -9,23 +8,30 @@ class Home extends React.Component {
 		this.processInput = this.processInput.bind(this);
 
 		this.state = {
-			val : '',
-		}
+			val: '',
+		};
 
 	}
    
     processInput(e) {
     	e.preventDefault();
-    	this.setState({val: this.test.value});
+    	this.setState({
+    		val : this.test.value
+    	});
     }
 
     render() {
         return  <div className="home">
-	        		<h2>Deepak's Homepage</h2>
+	        		<h2 >Deepak's Homepage</h2>
 	        		<div className="well">
-						<input  onKeyUp= {this.processInput} type="text" placeholder = "helloReact"  ref = {(elementAsArg) => {this.test = elementAsArg}}  />
+	        			<h2 className="text-primary">Live editing with state example</h2>
+	        			<div className="well ">
+        					<textarea rows="1" cols="50" placeholder = "type something here" onKeyUp = {this.processInput} ref = {(input) => {this.test = input}}  ></textarea>
+	        			</div>
+	        			<div className="well "><h5><p>See it here</p></h5> <p className="fonta">{this.state.val}</p> </div>
+	        			<div className="well "><h5><p>and here</p></h5> <p className="fontb">{this.state.val}</p> </div>
+	        			<div className="well "><h5><p>and here</p></h5> <p className="fontc">{this.state.val}</p> </div>
 	        		</div>
-					<div className="well">{this.state.val}</div>
         		</div>
     }
 }
